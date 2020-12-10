f25d1996fdebcd14acf8307ddeb33018d88d0627
Arden Collier <ardencollier1297@gmail.com>
2020-12-10 09:45:27 -0500

    new commit

--- client/src/components/chat/Chat.js
+++ client/src/components/chat/Chat.js
@@ -16,7 +16,7 @@ const Chat = ({ location }) => {
     const [message, setMessage] = useState('');
     const [messages, setMessages] = useState([]);
     // const [topArtists, setArtists] = useState([]);
-    const ENDPOINT = 'localhost:5000';
+    const ENDPOINT = 'https://react-spotify-chat-application.herokuapp.com/';
     const topArtists = []
 
     useEffect(() => {
